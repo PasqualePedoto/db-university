13e7eef6eecb48bf85cd21e5f9887d5eeffe93f9
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-09-07 15:56:33 +0200

    Added solution 2

--- join.sql
+++ join.sql
@@ -11,7 +11,13 @@ WHERE `degrees`.`name` = 'Corso di Laurea in Economia';
 
 -- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
 
-
+SELECT 
+`degrees`.*, 
+`departments`.`name` AS 'department' 
+FROM `departments` 
+JOIN `degrees` 
+ON `degrees`.`department_id` = `departments`.`id` 
+WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
 
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
 
